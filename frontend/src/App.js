fc3d4af8f8466e1637395e62ee0bccaa4a78c097
John Nguyen <jn2814@columbia.edu>
2024-04-13 11:09:40 -0400

    D3 samples and daily scheduler script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './App.css';
 import Home from "./components/Home";
+import Viz from "./components/Viz";
+import VizSample from "./components/VizSample";
 
 
 
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId='683899391622-a6kii8cr2sb4ov9olhqhngjhsl7eka2v.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId='132019401240-5mgdpd88e5ro256pofe8r1p9srqo518p.apps.googleusercontent.com'>
       <Router>
         <div>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/Viz" element={<Viz />} />
+              <Route path="/VizSample" element={<VizSample />} />
             </Routes>
         </div>
       </Router>
